1a5cf0697cc3369ef10d250331400056f27773a6
ayishahanna <ayishahanna775@gmail.com>
2024-04-20 19:51:43 +0530

    c

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mini/auth/Login.dart';
 import 'package:mini/firebase_options.dart';
-import 'package:hive/hive.dart';
-import 'package:mini/models/chat_message.dart';
-import 'package:mini/models/user_model.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final appDocumentDirectory =
-      await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDirectory.path);
-  await Hive.openBox<UserModel>('users');
-  await Hive.openBox<ChatMessage>('chat_messages');
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
--- lib/models/chat_message.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hive/hive.dart';
-
-@HiveType(typeId: 1)
-class ChatMessage {
-  @HiveField(0)
-  late String message;
-}
